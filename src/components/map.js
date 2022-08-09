d217e1af93e9acda8a0b955e2d57f8102919e499
kaleab007 <abatenehkaleab@gmail.com>
2022-08-08 22:10:43 -0700

    changes

--- src/components/map.js
+++ src/components/map.js
@@ -15,6 +15,19 @@ fetch('https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=37.219940108
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
 
-
+	const options = {
+		method: 'POST',
+		
+		headers: {
+			'X-RapidAPI-Key': API_Key,
+			'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+		}
+	};
+	
+	fetch('https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=37.21994010863107&lon=-122.06889194045178&page=2&radius=30', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+		.catch(err => console.error(err));
+	
 	
     export default Map;
