ca052f5dd343ccbcb0b21d0706124d60943cb49e
kaleab007 <abatenehkaleab@gmail.com>
2022-08-09 21:04:09 -0700

    switching up the search

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -4,7 +4,7 @@ import Map from './Map'
 
 //incorporating search bar to the second webpage 
 function App();
- {
+{
   let [searchTerm, setSearchTerm] = useState('')
   let [data, setData] = useState([])
   let [message, setMessage] = useState('Search for Trails!')
@@ -23,8 +23,9 @@ function App();
         }
       }
       fetchData()
-    },
-     [searchTerm])
+    }
+      
+    [searchTerm]; 
 
     const handleSearch = (e, term) => {
       e.preventDefault()
@@ -36,36 +37,8 @@ function App();
         {message}
         <Map data={data} />
       </div>
-    );
-  }
-//   return (
-//     <div className="page2">
-//       <div className="container">
-//         <div className="row align-items-center my-4">
-//           <div className="col-sm-6 pl-4 my-5">
-//             <h5>Repository link</h5>
-//             <a href={"https://github.com/RyanHPaez/hiking_for_stars"}>
-//               <h4>Check it out!</h4>
-//             </a>
-//             <div className="col-sm-10 my-5">
-//               <h1 className="font-weight-light">whatever we want!</h1>
-//               <p>paragraph can go here!:</p>
-//               <p>
-//                 paragraph can go here!
-//               </p>
-//             </div>
-//           </div>
-//           <div className= 'col-sm-6 '>          
-//             <img
-//               className='p'
-//               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xksVRek8uJJPEn-2VhLDD8PoOHI93xSAVQ&usqp=CAU"
-//               alt="hill"
-//             />          
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-export default App;
+    )
+  
+    
+  export default Pages2;
+    });
