98994d458a89dd3e8b53dd4fa97e4c3fd894fdf7
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-09 17:36:02 -0700

    added index.js for place and user schema

---
+++ src/backend/models/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const req = require('express/lib/request')
+const mongoose = require('mongoose')
+
+mongoose.connect(proces.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+module.exports.Place = require('./PlacesSchema')
+module.exports.User = require('./UserSchema.js')
