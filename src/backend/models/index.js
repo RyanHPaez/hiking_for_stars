1ae5b36ff56e3093638bd8147197a58cceb53c19
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-09 17:58:02 -0700

    removed seeder file mongodb updated with mock db

--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const req = require('express/lib/request')
 const mongoose = require('mongoose')
 
-mongoose.connect(proces.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
