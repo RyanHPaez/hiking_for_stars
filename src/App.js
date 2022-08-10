264827ef8188b17766caa0581b3c2f49c145c584
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-09 23:11:40 -0700

    saving current changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Navigation, Home, Page2, Page3, Page4 } from "./components";
-import React, {useContext, useEffect, useState } from "react";
+import { Navigation, Home, Page2, Page3, Page4} from "./components";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { PlaceDataContext } from "./context/placeDataContext";
+import { Places } from "./components"
 
 
 function App() {
@@ -13,11 +14,20 @@ function App() {
     axios
       .get(`http:localhost:3005/app/places`)
       .then ((response) => setPlaceData(response.data));
-  })
-  
+  }, [])
+  // const getData = async() => {
+  //   const {placeData} =await axios.get(`http:localhost:3005/app/places`);
+  //   setPlaceData(placeData)
+  // }
+
+  // useEffect(() => {
+  //   getData();
+  // }, [])
+
+
   return (
     <div className="App">
-      <PlaceDataContext.Provider value={placeData0}>
+      <PlaceDataContext.Provider value={placeData}>
       <Router>
         <Navigation />
         <Routes>
@@ -25,11 +35,12 @@ function App() {
           <Route path="/page2" element={<Page2 />} />
           <Route path="/page3" element={<Page3 />} />
           <Route path="/page4" element={<Page4 />} />
+          <Route path="/places" element={<Places />} />
         </Routes>
       </Router>
       </PlaceDataContext.Provider>
     </div>
-  );
+  )
 }
 
 export default App;
