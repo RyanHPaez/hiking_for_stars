eddaa2171a8be71d6ee3ae22f942787352116a4c
kaleab007 <abatenehkaleab@gmail.com>
2022-08-10 21:19:19 -0700

    functions

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -7,22 +7,23 @@ function Page2() {
   let [searchTerm, setSearchTerm] = useState('')
   let [data, setData] = useState([])
   let [message, setMessage] = useState('Search for Trails!')
+  //  let [options] = Map
 
   //the api address
   useEffect(() => {
     if (searchTerm) {
       console.log(searchTerm)
   
-    //   const fetchData = async () => {
-    //     const response = await fetch('https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=37.21994010863107&lon=-122.06889194045178&page=2&radius=30', options)
-    //     const resData = await response.json()
-    //     if(resData.results.length > 0) {
-    //       setData(resData.results)
-    //     } else {
-    //       setMessage('Not Found')
-    //     }
-    //   }
-    //   fetchData()
+      const fetchData = async () => {
+        const response = await fetch('https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=37.21994010863107&lon=-122.06889194045178&page=2&radius=30', )
+        const resData = await response.json()
+        if(resData.results.length > 0) {
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
+        }
+      }
+      fetchData()
      } 
     }); 
     
