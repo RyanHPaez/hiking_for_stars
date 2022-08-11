7dabbf73cf0f012243cb1e7e54beaaff8ef07b85
RyanHPaez <solosnail@gmail.com>
2022-08-10 20:25:29 -0700

    database

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,5 @@
-require('dotenv').config();
-const express = require("express");
-const app = express();
 const mongoose = require('mongoose')
-// const cors = require('cors');
-const uri = process.env.MONGO_URI
 
-
-
-//connect to mongoose
-mongoose.connect('mongodb+srv://newUser1:8570Abr@h@m09@cluster0.mupyl.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: on PORT 3005') })
-
-  
-
-//Middleware
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'))
-// app.use(cors())
-
-const placeController = require('./controller/PlaceController');
-app.use('/app', placeController)
-
-app.get('/', (req, res) =>{
-  res.send('Hello World')
-})
-app.listen(3005, () => console.log("Server is running on PORT 3005"));
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+  )
