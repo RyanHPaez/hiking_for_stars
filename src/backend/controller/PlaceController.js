a26cdb40418fef5c8e2112079dc0a59fe0c3d428
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-10 20:27:13 -0700

    restoring last push

--- src/backend/controller/PlaceController.js
+++ src/backend/controller/PlaceController.js
@@ -4,17 +4,17 @@ const Place = require('../models/PlacesSchema');
 
 router.get('/places', async (req, res) => {
 
-    console.log('inside the places route')
-    const foundPlaces = await Place.find()
-    console.log(foundPlaces)
+    // console.log('inside the places route')
+    // const foundPlaces = await Place.find()
+    // console.log(foundPlaces)
     // res.send('inside theplaces')
     // console.log(Place.find())
-    // try{
-    //   const foundPlaces = await Place.find();
-    //   res.status(200).json(foundPlaces)
-    //   }catch(err){
-    //     res.status(500).json(err)
-    //   }
+    try{
+      const foundPlaces = await Place.find();
+      res.status(200).json(foundPlaces)
+      }catch(err){
+        res.status(500).json(err)
+      }
     }
 )
 
