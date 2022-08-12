37945adf3925f8c1d85b03dc3223189982da8925
RyanHPaez <solosnail@gmail.com>
2022-08-12 16:47:27 -0700

    fixing page2

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -4,22 +4,38 @@ function Page2() {
   return (
     <div className="page2">
       <div className="container">
-        <div className="row align-items-center my-4">
-          <div className="col-sm-6 pl-4 my-5">
-            <div className="col-sm-8 my-5">
-              <h1 className="font-weight-light">
-                Search your favorite hiking spot!
-              </h1>
-              <p>paragraph can go here!:</p>
-              <p>paragraph can go here!</p>
+        <div className="row align-items-start my-4">
+          <div className="row">
+            <div className="col-{breakpoint}-auto">
+              <form>
+                <input
+                  // ref={term}
+                  type="text"
+                  placeholder="Where to?"
+                  // onChange={(e)=>setInput(e.target.value)}
+                />
+                <button>
+                  <i class="fas fa-search"></i>
+                </button>
+              </form>
+              <div className="row align-items-center">
+                <div className="col-{breakpoint}-auto">
+                  <h1 className="font-weight-light">
+                    Search your favorite hiking spot!
+                  </h1>
+                  <p>paragraph can go here!</p>
+                </div>
+              </div>
+              <div className="row align-items-end">
+                <div className="col-md-12">
+                  <img
+                    className="img-fluid "
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xksVRek8uJJPEn-2VhLDD8PoOHI93xSAVQ&usqp=CAU"
+                    alt="hills"
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="col-sm-4 mr-5 ">
-            <img
-              className="p"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xksVRek8uJJPEn-2VhLDD8PoOHI93xSAVQ&usqp=CAU"
-              alt="hill"
-            />
           </div>
         </div>
         <h5>Repository link</h5>
