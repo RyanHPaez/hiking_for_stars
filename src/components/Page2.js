cce57aea650abec0197e218e999c65f1403187c8
RyanHPaez <solosnail@gmail.com>
2022-08-12 13:12:22 -0700

    page2 switch to grids

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -2,26 +2,36 @@ import React from "react";
 
 function Page2() {
   return (
-    <div className="page2">
+    <div className="grid-container">
       <div className="container">
-        <div className="row align-items-center my-4">
-          <div className="col-sm-6 pl-4 my-5">
-            <div className="col-sm-8 my-5">
-              <h1 className="font-weight-light">
-                Search your favorite hiking spot!
-              </h1>
-              <p>paragraph can go here!:</p>
-              <p>paragraph can go here!</p>
-            </div>
-          </div>
-          <div className="col-sm-4 ml-5 ">
-            <img
-              className="p"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xksVRek8uJJPEn-2VhLDD8PoOHI93xSAVQ&usqp=CAU"
-              alt="hill"
+        <div className="grid-item pl-4 my-5">
+          <form>
+            <input
+              // ref={term}
+              type="text"
+              placeholder="Where to?"
+              // onChange={(e)=>setInput(e.target.value)}
             />
+            <button>
+              <i class="fas fa-search"></i>
+            </button>
+          </form>
+          <div className="grid-item ">
+            <h1 className="font-weight-light">
+              Search your favorite hiking spot!
+            </h1>
+            <p>paragraph can go here!</p>
           </div>
         </div>
+        <div className="imgpage2">
+          <img
+            className="p grid-item ml-5 "
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xksVRek8uJJPEn-2VhLDD8PoOHI93xSAVQ&usqp=CAU"
+            alt="hill"
+          />
+        </div>
+      </div>
+      <div className="grid-item">
         <h5>Repository link</h5>
         <a href={"https://github.com/RyanHPaez/hiking_for_stars"}>
           <h4>Check it out!</h4>
