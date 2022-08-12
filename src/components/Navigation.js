951bc6c93585f48e11a7a66996932dee41530f00
RyanHPaez <solosnail@gmail.com>
2022-08-11 23:16:22 -0700

    nav text shadows

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ function Navigation() {
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
-          <i>Hike with the stars</i>
+          <i>Hiking for Stars</i>
           </NavLink>
           <div>
             <ul className="navbar-nav ml-auto">
