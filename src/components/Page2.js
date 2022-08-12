60d0cd4819225e2237a799b1088d33484074e8a4
RyanHPaez <solosnail@gmail.com>
2022-08-12 13:15:31 -0700

    page2 switch to grids

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -4,7 +4,8 @@ function Page2() {
   return (
     <div className="grid-container">
       <div className="container">
-        <div className="grid-item pl-4 my-5">
+        <div className="row align-items-center my-4">
+          <div className="grid-item pl-4 my-5">
           <form>
             <input
               // ref={term}
@@ -16,26 +17,27 @@ function Page2() {
               <i class="fas fa-search"></i>
             </button>
           </form>
-          <div className="grid-item ">
-            <h1 className="font-weight-light">
-              Search your favorite hiking spot!
-            </h1>
-            <p>paragraph can go here!</p>
+            <div className="grid-item ">
+              <h3 className="font-weight-light">
+                Search your favorite hiking spot!
+              </h3>
+              <p>paragraph can go here!</p>
+            </div>
+          </div>
+          <div className="imgpage2">
+            <img
+              className="p grid-item ml-5 "
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xksVRek8uJJPEn-2VhLDD8PoOHI93xSAVQ&usqp=CAU"
+              alt="hill"
+            />
           </div>
         </div>
-        <div className="imgpage2">
-          <img
-            className="p grid-item ml-5 "
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3xksVRek8uJJPEn-2VhLDD8PoOHI93xSAVQ&usqp=CAU"
-            alt="hill"
-          />
-        </div>
-      </div>
-      <div className="grid-item">
+        <div className= "grid-item">
         <h5>Repository link</h5>
         <a href={"https://github.com/RyanHPaez/hiking_for_stars"}>
           <h4>Check it out!</h4>
         </a>
+        </div>
       </div>
     </div>
   );
