129a3104788fb22de564e3ff40e198b06c96aa60
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-12 22:18:15 -0700

    homepage updated. mapping through data and displaying tiled information

--- src/components/Home.js
+++ src/components/Home.js
@@ -45,124 +45,28 @@ const favoritedTrail = (trail) => {
   })
 
   return (
+      <div>
+        <div>
+          <h5>Trails: {display}</h5>
+        </div>
+        <div>
+          <TrailDetails trigger = {hoverPopup} setTrigger={setHoverPopup}>
+              <h3 className='trailName'>{favoritedTrail.name}</h3>
+              <img className='detailViewImg' src={favoritedTrail.thumbnail} ></img>
+              <div className='detailDescription'>
+                
+                <span className='description'>{favoritedTrail.description}</span>
+                <br/>
+                Rating: {favoritedTrail.rating}<br/>
+                Difficulty: {favoritedTrail.difficulty}<br/>
+                Address: {favoritedTrail.address}<br/>
+                City: {favoritedTrail.city}<br/>
 
-    
-    <div className="home">
-      <div className="container">
-        <PlaceDataContext.Provider>
-        <TrailDetails trigger={hoverPopup} setTrigger={setHoverPopup} src={placeData.thumbnail}>
-          <div className="col-sm-12  my-5">
-            <h1 className="font-weight-light ">Let's go HIKING!</h1>
-            <form>
-              <input
-                // ref={term}
-                type="text"
-                placeholder="Where to?"
-                // onChange={(e)=>setInput(e.target.value)}
-              />
-              <button>
-                <i class="fas fa-search"></i>
-              </button>
-            </form>
-            <p>Are we ready to put text here?</p>
-          </div>
-          <div className="row align-items-center my-5">
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[0].thumbnail}
-                  alt="hills"
-                  onMouseOver={() => {setHoverPopup(true);}}
-                  onMouseOut={() =>{setHoverPopup(false)}}
-                ></img>
-              </button>
-              <p>{placeData[0].name}</p>
-            </div>
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[1].thumbnail}
-                  alt="hills"
-                />
-              </button>
-              <p>{placeData[1].name}</p>
-            </div>
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[2].thumbnail}
-                  alt="hills"
-                />
-              </button>
-              <p>{placeData[2].name}</p>
-            </div>
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[3].thumbnail}
-                  alt="hills"
-                />
-              </button>
-              <p>{placeData[3].name}</p>
-            </div>
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[4].thumbnail}
-                  alt="hills"
-                />
-              </button>
-              <p>{placeData[4].name}</p>
-            </div>
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[5].thumbnail}
-                  alt="hills"
-                />
-              </button>
-              <p>{placeData[5].name}</p>
-            </div>
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[6].thumbnail}
-                  alt="hills"
-                />
-              </button>
-              <p>{placeData[6].name}</p>
-            </div>
-            <div className="col-md-3 my-2">
-              <button>
-                <img
-                  className="img-fluid "
-                  src={placeData[7].thumbnail}
-                  alt="hills"
-                />
-              </button>
-              <p>{placeData[7].name}</p>
-            </div>
-
-            <div className="col-sm-12 my-4">
-              <h1 className="font-weight-light">About:</h1>
-              <p>more Text or paragraph here!</p>
-              <p>- Team Hangout</p>
-            </div> 
-          </div>
-          {/* {displayPlaces} */}
-        </TrailDetails>
-        </PlaceDataContext.Provider>
+              </div>
+          </TrailDetails>
+        </div>
       </div>
-      
-    </div>
   );
 }
-
+   
 export default Home;
