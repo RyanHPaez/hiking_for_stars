76efdbdc7d458e4cf2e3e380a85da708e3aeaee9
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-12 23:52:27 -0700

    updated userSchema to include required and unique fields. setting up for db

--- src/backend/models/UserSchema.js
+++ src/backend/models/UserSchema.js
@@ -2,14 +2,25 @@ const mongoose = require ('mongoose')
 
 // schema
 const userSchema = new mongoose.Schema({
-  name: { type: String },
-  password: { type: String },
+  name: { 
+      type: String,
+      required: true, 
+      unique: true
+  },
+  email: {
+      type: String,
+      required: true,
+      unique: true
+  },
+  password: { 
+      type: String,
+      required: true,
+  },
   favoriteTrails: { type: [{ type: String }] },
   previouslyVisited: { type: [{ type: String }] },
   expLevel: { type: String },
-  email: {type: String},
   image: { type: String, default: 'http://placehold.it/500x500.png'}
-});
+}, {timestamps: true});
 
 // model and export 
 module.exports = mongoose.model('User', userSchema)
