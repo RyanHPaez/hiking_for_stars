bab52383e01753bbaafed54c7c4529f00e6df54d
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-12 23:47:06 -0700

    added login page. still pending work. waiting on db setup to finish connecting

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+function Login ({setWelcomeUser}) {
+
+    let navigate = userNavigate();
+    const [userData, setUserData] = useData([]);
+    const [userName, setUserName] = useState("");
+
+
+
+    
+}
