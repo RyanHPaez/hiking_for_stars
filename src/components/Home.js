0246e1e063a8131f9b4508c43a12c9afdad9b9e9
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-12 23:04:22 -0700

    added a add to favorites button to the homepage render. only produces a window alert at the moment. will connect to route later

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,52 @@ import TrailDetails from './TrailDetails';
 
 
 function Home() {
-
+  const [userFavorite, setUserFavorite ] = useState([])
   const [hoverPopup, setHoverPopup] = useState(false);
   
   useEffect( () => {}, []);
 
   const placeData = useContext(PlaceDataContext)
  
-//favorited trail
-const favoritedTrail = (trail) => {
-    window.alert(`Added ${trail.name} to your favorites!`)
-};
+  //favorited trail
+  const favoritedTrail = (trail) => {
+      setUserFavorite(trail)
+      window.alert(`Added ${trail.name} to your favorites!`)
+  };
 
-  //map through data and render images and information for page
+  const handleClick = (e) => {
+    e.preventDefault();
+    let newFave = {};
+    newFave = userFavorite;
+  }
+
+  const displayDetail = ((item, i) =>{
+    return (
+      <div
+      style={{
+        background: "black",
+        width: "280px",
+        height: "200px",
+        margin: "20px",
+        textAlign: "center",
+        display: "inline-table",
+        backgroundColor: "black",
+        color: "white",
+        borderRadius: "20px",
+        position: "relative",
+      }}
+    >
+      <form>
+        <h2 key={i}>{item.title}</h2>
+        <img src={item.gameImage}></img>
+        <p>${item.price}</p>
+       
+      </form>
+    </div>
+    )
+  })
+
+
   const display = placeData.map((item, i) => {
 
     return(
@@ -37,7 +70,7 @@ const favoritedTrail = (trail) => {
       >
           <form >
             <h2 key={i}>{item.name}</h2>
-            <img src={item.thumbnail} onMouseOver={() => {setHoverPopup(true)}}
+            <img src={item.thumbnail} onMouseOver={() => {setHoverPopup(true); displayDetail(item)}}
             onMouseOut={() => {setHoverPopup(false)}}></img>
             <p><b>Rating: </b>{item.rating}<b/>
             <button onClick={() => {
@@ -71,18 +104,23 @@ const favoritedTrail = (trail) => {
           </div>
           <h5>Trails: {display}</h5>
         </div>
+        <div className="col-sm-12 my-4">
+              <h1 className="font-weight-light">About:</h1>
+              <p>more Text or paragraph here!</p>
+              <p>- Team Hangout</p>
+        </div> 
         <div>
-          <TrailDetails trigger = {hoverPopup} setTrigger={setHoverPopup} src={favoritedTrail.thumbnail}>
-              <h3 className='trailName'>{favoritedTrail.name}</h3>
-              <img className='detailViewImg' src={favoritedTrail.thumbnail} ></img>
+          <TrailDetails trigger = {hoverPopup} setTrigger={setHoverPopup} src={placeData.thumbnail}>
+              <h3 className='trailName'>{placeData.name}</h3>
+              <img className='detailViewImg' src={placeData.thumbnail} ></img>
               <div className='detailDescription'>
                 
-                <span className='description'>{favoritedTrail.description}</span>
+                <span className='description'>{placeData.description}</span>
                 <br/>
-                Rating: {favoritedTrail.rating}<br/>
-                Difficulty: {favoritedTrail.difficulty}<br/>
-                Address: {favoritedTrail.address}<br/>
-                City: {favoritedTrail.city}<br/>
+                Rating: {placeData.rating}<br/>
+                Difficulty: {placeData.difficulty}<br/>
+                Address: {placeData.address}<br/>
+                City: {placeData.city}<br/>
 
               </div>
           </TrailDetails>
