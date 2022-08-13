84c654c16f1c5a054aff2b1944911228dc6dff19
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-08-12 23:37:19 -0700

    updated userSchema to include favorites, previous visits and experience level

--- src/backend/models/UserSchema.js
+++ src/backend/models/UserSchema.js
@@ -4,6 +4,9 @@ const mongoose = require ('mongoose')
 const userSchema = new mongoose.Schema({
   name: { type: String },
   password: { type: String },
+  favoriteTrails: { type: [{ type: String }] },
+  previouslyVisited: { type: [{ type: String }] },
+  expLevel: { type: String },
   email: {type: String},
   image: { type: String, default: 'http://placehold.it/500x500.png'}
 });
