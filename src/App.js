c4b233489a7342a8b2c0f0c2924ffc0f3bec371e
RyanHPaez <solosnail@gmail.com>
2022-08-15 16:29:34 -0700

    css-ryan

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Navigation, Home, Page2, Music, Login } from "./components";
+import { Navigation, Home, Page2, Music, Login, SignUp } from "./components";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { PlaceDataContext } from "./context/placeDataContext";
-import { Places } from "./components"
-
-
-
-
+import { Places } from "./components";
 
 function App() {
   const [placeData, setPlaceData] = useState([]);
   // const [userData, setUserData] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     axios
-      .get('http://localhost:3005/app/places')
-      .then ((response) => setPlaceData(response.data));
+      .get("http://localhost:3005/app/places")
+      .then((response) => setPlaceData(response.data));
   }, []);
 
   //welcome user
-  const [welcomeUser, setWelcomeUser]= useState('Please log in');
-  const logInLogOut = welcomeUser === 'Please log in' ? <a href='/Login'></a> : <a href='#'></a>
+  const [welcomeUser, setWelcomeUser] = useState("Please log in");
+  const logInLogOut =
+    welcomeUser === "Please log in" ? <a href="/Login"></a> : <a href="#"></a>;
 
   return (
     <div className="App">
@@ -35,12 +32,13 @@ function App() {
             <Route path="/page2" element={<Page2 />} />
             <Route path="/Music" element={<Music />} />
             <Route path="/Login" element={<Login />} />
+            <Route path="/SignUp" element={<SignUp />} />
             <Route path="/places" element={<Places />} />
           </Routes>
         </Router>
       </PlaceDataContext.Provider>
     </div>
-  )
+  );
 }
 
 export default App;
