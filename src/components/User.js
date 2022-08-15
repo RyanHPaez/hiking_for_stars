e9a657e81dbda35364af56ea00b92133b195ad32
Natalia <natyargumedo@hotmail.com>
2022-08-15 11:55:56 -0700

    working on adding user page

---
+++ src/components/User.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from "react";
+import {useParams, useNavigate} from 'react-router-dom';
+
+
+function User(){
+    return(
+        <>
+       <div className="profile">
+           <div className="profileRight">
+               <div className="profileRightTop">
+                    <div className="profileCover">
+               <img className="profileCoverImg" src='assets/forest.jpg'/>
+                    <img className="profileUser" src='assets/cat.jpg'/> 
+                    </div>
+                    <div className="profileInfo">
+                        <h4 className="profileName">name</h4>
+                        <span className="profileBio">info</span>
+                    </div>
+               </div>
+           </div> 
+       </div>
+       </>
+    )
+}
+
+export default User;
