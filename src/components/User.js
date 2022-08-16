d7dedd3c69f8ed9c55a7bffd8dc1c6e249d8d09f
RyanHPaez <solosnail@gmail.com>
2022-08-15 23:45:35 -0700

    user page ready

--- src/components/User.js
+++ src/components/User.js
@@ -10,26 +10,42 @@ import {useParams, useNavigate} from 'react-router-dom';
 function User(){
     return(
         <>
-       <div className="profile">
-           <div className="profileRight">
-               <div className="profileRightTop">
-                    <div className="profileCover">
-
-               <img className="profileCoverImg" src='assets/forest.jpg'
-               alt=''/>
-                    <img className="profileUser" src='assets/cat.jpg'
-                    alt=''/> 
-
-                    </div>
-                    <div className="profileInfo">
-                        <h4 className="profileName">name</h4>
-                        <span className="profileBio">info</span>
-                    </div>
-               </div>
-           </div> 
-       </div>
+      <div className="contact">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="img col-sm-12 shadow-lg p-1 mb-1">
+            <a href={"https://www.linkedin.com/in/ryan-paez/"}>
+              
+                
+                <img className="profileUser rounded-circle my-3" src='assets/cat.jpg'
+                alt=''/> 
+            </a>
+            <div className="col-sm-12">
+              
+              <h4 className="profileName font-weight-light text-center">name</h4>
+              <div className="text-center"></div>
+            </div>
+          </div>
+          <div className="col-sm-12 my-4">
+            <h1 className="font-weight-light text-center">About Me</h1>
+            <p className="text-center">
+            <span className="profileBio">info</span>
+            </p>
+            <p>- I am a hiker</p>
+          </div>
+        </div>
+      </div>
+    </div>
        </>
     )
 }
 
 export default User;
+{/* <img className="profileCoverImg" src='assets/forest.jpg'
+               alt=''/>
+                    
+
+                    </div>
+                    <div className="profileInfo">
+                        
+                         */}
