41a33c1073e0cec1963816ee7422f729afe9c22d
RyanHPaez <solosnail@gmail.com>
2022-08-16 15:41:48 -0700

    user heroku

--- src/backend/models/UserSchema.js
+++ src/backend/models/UserSchema.js
@@ -1,56 +1,56 @@
-const mongoose = require ('mongoose')
+// const mongoose = require ('mongoose')
 
-// schema
-const userSchema = new mongoose.Schema({
-  user_name: { 
-      type: String,
-      required: true, 
-      unique: true
-  },
+// // schema
+// const userSchema = new mongoose.Schema({
+//   user_name: { 
+//       type: String,
+//       required: true, 
+//       unique: true
+//   },
   
-  first_name:{
-    type: String,
-    required: true,
-    unique: true
-},
+//   first_name:{
+//     type: String,
+//     required: true,
+//     unique: true
+// },
 
-  last_name: {
-  type: String,
-  required: true,
-  unique: true
-}, 
+//   last_name: {
+//   type: String,
+//   required: true,
+//   unique: true
+// }, 
 
-  email: {
-      type: String,
-      required: true,
-      unique: true
-  },
+//   email: {
+//       type: String,
+//       required: true,
+//       unique: true
+//   },
   
-  password: { 
-      type: String,
-      required: true,
-  },
+//   password: { 
+//       type: String,
+//       required: true,
+//   },
   
-  profile_id: {
-    type: Number
-  },
+//   profile_id: {
+//     type: Number
+//   },
   
-  registered_at: {
-    type: Date
-  },
+//   registered_at: {
+//     type: Date
+//   },
   
-  updatedAt: {
-    type: Date
-  },
+//   updatedAt: {
+//     type: Date
+//   },
   
-  favoriteTrails: { type: [{ type: String }] },
+//   favoriteTrails: { type: [{ type: String }] },
   
-  previouslyVisited: { type: [{ type: String }] },
+//   previouslyVisited: { type: [{ type: String }] },
   
-  expLevel: { type: String },
+//   expLevel: { type: String },
   
-  image: { type: String, default: 'http://placehold.it/500x500.png'}
-}, {timestamps: true});
+//   image: { type: String, default: 'http://placehold.it/500x500.png'}
+// }, {timestamps: true});
 
-// model and export 
-module.exports = mongoose.model('User', userSchema)
+// // model and export 
+// module.exports = mongoose.model('User', userSchema)
