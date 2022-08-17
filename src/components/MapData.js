81ac11741a37c5424c688da273544702b315091e
kaleab007 <abatenehkaleab@gmail.com>
2022-08-17 12:09:47 -0700

    deleted show.js

--- src/components/MapData.js
+++ src/components/MapData.js
@@ -7,33 +7,23 @@ import React, { useState } from "react";
 
 function MapData(props){
 	
-	
-	
-	
-	
-    // const display = props.map((item, index) => {
-    //      return (
-	// 	<div>
-
-		
-	// 		<div className='trailName' key= {index}><p>{item.name}</p></div>
-	// 	    <img src= {item.thumbnail} alt= '' default></img>
-	// 		<div className= 'trailCity' ><p>{item.city}</p></div>
-	// 		<div className= 'trailDifficulty'><p>{item.difficulty }</p></div>
-	// 		<div className='trailDirections'><p>{item.directions}</p></div>
-	// 		<div className='trailDescription'><p>{item.description}</p></div>
-           
-	// 	</div>
-		
-           
-    //     )
-    // })
-
-    return (
+	const display = props.map((item, index) => {
+         return (
+		<div>
+			<div className='trailName' key= {index}><p>{item.name}</p></div>
+		    <img src= {item.thumbnail} alt= '' default></img>
+			<div className= 'trailCity' ><p>{item.city}</p></div>
+			<div className= 'trailDifficulty'><p>{item.difficulty }</p></div>
+			<div className='trailDirections'><p>{item.directions}</p></div>
+			<div className='trailDescription'><p>{item.description}</p></div>
+           </div>
+		    )
+    })
+ return (
 		console.log(props)
-        // <div>
-        //     {/* {display} */}
-        // </div>
+        <div>
+            {display}
+        </div>
     )
 }
 
