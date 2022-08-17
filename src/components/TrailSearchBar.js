07af52084fc376c0428e37e90ff09c95b857a8b6
kaleab007 <abatenehkaleab@gmail.com>
2022-08-16 22:03:42 -0700

    props

--- src/components/TrailSearchBar.js
+++ src/components/TrailSearchBar.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 function TrailSearchBar(props){
-    const [searchTerm,setSearchTerm] = useState('')
+    const [latitude, setLatitude] = useState('')
+    const [longitude, setLongitude] = useState('')
+
     return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-            <input type="text" placeholder="Input Latitude" onChange={(e)=> setSearchTerm(e.target.value)}/>
-            <input type="text" placeholder="Input Longitude" onChange={(e)=> setSearchTerm(e.target.value)}/>
+        <form onSubmit={(e) => props.handleSearch(e, latitude,longitude)}>
+            <input type="text" placeholder="Input Latitude" onChange={(e)=> setLatitude(e.target.value)}/>
+            <input type="text" placeholder="Input Longitude" onChange={(e)=> setLongitude(e.target.value)}/>
             <input type="Submit"></input>
         </form>
     )
