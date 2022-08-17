0a7b2f2d2eb5a5bf3b7d3294a45bfdede4c21ed1
kaleab007 <abatenehkaleab@gmail.com>
2022-08-17 16:30:37 -0700

    com

--- src/components/map.js
+++ src/components/map.js
@@ -97,9 +97,9 @@ function Map() {
  
 
   return (
-    <div className="col-sm-12 my-5">
+    <div className="App">
       <TrailSearchBar handleSearch={handleSearch} />
-    <h2 className="font-weight-light">Search for Trails!</h2> 
+    <h2>Search for Trails!</h2> 
       <MapData data={data} />
     </div>
   )
