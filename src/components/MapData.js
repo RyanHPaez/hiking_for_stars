ece3cb141f52dbfa9e9299f078bd78452ebac888
RyanHPaez <solosnail@gmail.com>
2022-08-17 12:27:40 -0700

    map fix

--- src/components/MapData.js
+++ src/components/MapData.js
@@ -7,34 +7,24 @@ import React, { useState } from "react";
 
 function MapData(props){
 	
-	
-	
-	
-	
-    // const display = props.map((item, index) => {
-    //      return (
-	// 	<div>
-
-		
-	// 		<div className='trailName' key= {index}><p>{item.name}</p></div>
-	// 	    <img src= {item.thumbnail} alt= '' default></img>
-	// 		<div className= 'trailCity' ><p>{item.city}</p></div>
-	// 		<div className= 'trailDifficulty'><p>{item.difficulty }</p></div>
-	// 		<div className='trailDirections'><p>{item.directions}</p></div>
-	// 		<div className='trailDescription'><p>{item.description}</p></div>
-           
-	// 	</div>
-		
-           
-    //     )
-    // })
-
-    return (
-		console.log(props)
-        // <div>
-        //     {/* {display} */}
-        // </div>
-    )
+	const display = props.map((item, index) => {
+         return (
+		<div>
+			<div className='trailName' key= {index}><p>{item.name}</p></div>
+		    <img src= {item.thumbnail} alt= '' default></img>
+			<div className= 'trailCity' ><p>{item.city}</p></div>
+			<div className= 'trailDifficulty'><p>{item.difficulty }</p></div>
+			<div className='trailDirections'><p>{item.directions}</p></div>
+			<div className='trailDescription'><p>{item.description}</p></div>
+           </div>
+		    )
+    })
+//  return (
+// 		console.log(props)
+//         <div>
+//             {display}
+//         </div>
+//     )
 }
 
 	 export default MapData;
