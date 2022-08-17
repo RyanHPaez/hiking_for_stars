57077c050d251ca7f49894e0830ac465bf84a600
kaleab007 <abatenehkaleab@gmail.com>
2022-08-17 11:26:31 -0700

    merge

--- src/components/map.js
+++ src/components/map.js
@@ -1,64 +1,108 @@
 import { useEffect, useState } from 'react'
 import TrailSearchBar from './TrailSearchBar'
 import MapData from './MapData'
-import TrailDetails from './TrailDetails';
-
-
 //incorporating search bar to the second webpage 
 function Map() {
-  // return(
-  //   <div className='Map'>
-  //   <div className='container'>
-  //   <div className='row align-items-start my-4'>
-  //     <div className='row'>
-  //       <div className='col-{breakpoint}-auto'>
-  //         <form>
-  //           <input 
-  //           type={text}
-  //           placeholder= "Where to?" />
-  //           <button>
-  //           <i class= "fas fa-search"></i>
-  //           </button>
-  //         </form>
-    const [searchTerm, setSearchTerm] = useState('')
-    const [data, setData] = useState([])
-    const[message, setMessage] = useState('Search for Trails!')
-    const options = Map
-    // </div>
-  
 
+  const [data, setData] = useState([])
+
+  const options = {
+    method: 'GET',
+    terms: {
+      type: String,
+      require: true,
+    },
+    id: {
+      type: String,
+      require: false,
+    },
+    url: {
+      type: String,
+      require: false,
+    },
+    name: {
+      type: String,
+      require: false,
+
+    },
+    city: {
+      type: String,
+      require: true,
+
+    },
+    region: {
+      type: String,
+      require: true,
+
+    },
+    country: {
+      type: String,
+      require: false,
+
+    },
+    difficulty: {
+      type: String,
+      require: true,
+
+    },
+    thumbnail: {
+      type: String,
+      require: false,
+
+    },
+    lon: {
+      type: Number,
+      require: Boolean,
+    },
+    lat: {
+      type: Number,
+      require: Boolean,
+    },
+    headers: {
+      'X-RapidAPI-Key': '1fc73e876cmshbbb7a9c22b1c966p17aa01jsn612c4925dd8f',
+      'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+    }
+  };
   //the api address
   useEffect(() => {
-    if (searchTerm) {
-      console.log(searchTerm)
-  
       const fetchData = async () => {
-      const response = await fetch('https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=37.21994010863107&lon=-122.06889194045178&page=2&radius=30', options)
+      const response = await fetch(`https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=37.21994010863107&lon=-122.06889194045178&radius=30`, options)
       const resData = await response.json()
-        if(resData.results.length > 0) {
+      // console.log(resData)
+        // if(resData.results.length > 0) {
           setData(resData.results)
-        } else {
-          setMessage('Not Found')
-        }
+        // } else {
+        //   setMessage('Not Found')
+        // }
       }
       fetchData()
-     } 
-    }); 
-    
+     }, [data]);
 
-    const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearchTerm(term)
-    }
-    
-    return (
-      <div className="App">
-        <TrailSearchBar handleSearch={handleSearch} />
-        {message}
-        <MapData data={data} />
-      </div>
-    );
-    }
-    
-  export default Map;
-    
+
+  const handleSearch = (e, longitude, latitude) => {
+    e.preventDefault()
+    console.log(longitude, latitude)
+     const fetchData = async () => {
+        const response = await fetch(`https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=${latitude}&lon=${longitude}&radius=30`, options)
+        const resData = await response.json()
+        console.log(resData)
+          // if(resData.results.length > 0) {
+          //   setData(resData.results)
+          // } else {
+          //   setMessage('Not Found')
+          // }
+        }
+        fetchData()
+       } 
+ 
+
+  return (
+    <div className="App">
+      <TrailSearchBar handleSearch={handleSearch} />
+    <h2>Search for Trails!</h2> 
+      <MapData data={data} />
+    </div>
+  )
+  }
+  
+export default Map;
