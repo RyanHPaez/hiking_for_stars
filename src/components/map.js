4809b2f2fe51aaf60dcc693023b85eaa0c44b3a8
RyanHPaez <solosnail@gmail.com>
2022-08-17 14:02:20 -0700

    css map page

--- src/components/map.js
+++ src/components/map.js
@@ -97,7 +97,7 @@ function Map() {
  
 
   return (
-    <div className="App">
+    <div className="col-sm-12 my-5">
       <TrailSearchBar handleSearch={handleSearch} />
     <h2>Search for Trails!</h2> 
       <MapData data={data} />
