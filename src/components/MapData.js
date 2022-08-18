8e7d8d4339c0c0f83e78ed7a967cfd78f8ee8b7f
kaleab007 <abatenehkaleab@gmail.com>
2022-08-18 15:56:49 -0700

    MapData

--- src/components/MapData.js
+++ src/components/MapData.js
@@ -1,24 +1,60 @@
 import { type } from "@testing-library/user-event/dist/type";
 import React, { useState } from "react";
 
-function MapData(props){
-	// const display = props.map((item, index) => {
-    //      return (
-	// 	<div>
+// function MapData(props){
+// 	//OLD FUNCTION//
+// 	const display = props.map((item, index) => {
+//          return (
+// 		<div>
 
 		
-	// 		<div className='trailName' key= {index}><p>{item.name}</p></div>
-	// 	    <img src= {item.thumbnail} alt= '' default></img>
-	// 		<div className= 'trailCity' ><p>{item.city}</p></div>
-	// 		<div className= 'trailDifficulty'><p>{item.difficulty }</p></div>
-	// 		<div className='trailDirections'><p>{item.directions}</p></div>
-	// 		<div className='trailDescription'><p>{item.description}</p></div>
+// 			<div className='trailName' key= {index}><p>{item.name}</p></div>
+// 		    <img src= {item.thumbnail} alt= '' default></img>
+// 			<div className= 'trailCity' ><p>{item.city}</p></div>
+// 			<div className= 'trailDifficulty'><p>{item.difficulty }</p></div>
+// 			<div className='trailDirections'><p>{item.directions}</p></div>
+// 			<div className='trailDescription'><p>{item.description}</p></div>
            
-	// 	</div>
+// 		</div>
 		
            
-    //     )
-    // })
+//         )
+//     })
+// //NEW FUNCTION//--RENDERING INFO--
+// import { type } from "@testing-library/user-event/dist/type";
+// import React, { useState } from "react";
+
+// function _renderObject(){
+// 	return Object.keys(ObjectTest). map(obj, i) => {
+//          return (
+// 		<div>
+			
+// 			name is:{ObjectTest[obj].name};
+// 			url is:{ObjectTest[obj].url};
+// 			city is:{ObjectTest[obj].city};
+// 			region is:{ObjectTest[obj].region};
+// 			country is:{ObjectTest[obj].country};
+// 			latitude is:{ObjectTest[obj].lat};
+// 			longitude is:{ObjectTest[obj].lon};
+// 			difficulty is:{ObjectTest[obj].difficulty};
+// 			features is:{ObjectTest[obj].features};
+// 			rating is:{ObjectTest[obj].rating};
+// 			thumbnail is:{ObjectTest[obj].thumbnail};
+// 		</div>
+// 		 )
+//  })
+// }
+// //     return (
+// // 		console.log(obj)
+// //         <div>
+// //             {display}
+// //         </div>
+// //     )
+// // }
+
+// //      export default _renderObject;
+
+
 
     return (
 		console.log(props)
