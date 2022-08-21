a3f57f61d02e032809c69473fd75700674127018
RyanHPaez <solosnail@gmail.com>
2022-08-21 16:36:38 -0700

    added back the proxy

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function SearchBar(props){
-    const[searchTerm, setSearchTerm] = useState('')
-    // const[searchTerm, setSearchTerm] = useState('')
-    return (
-        <div className="container">
-        <div className="row ">
-          <div className="col-sm-12 text-center my-4">
-            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-                <h1 className='font-weight-light'>Look up your favorite music for your trip</h1>
-                <input 
-                type="text" 
-                className="form-control"
-                placeholder="Search Here" 
-                onChange={(e) => setSearchTerm(e.target.value)} />
-                <input 
-                type="submit" 
-                />                
-            </form>
-            </div>
-            </div>
-            </div>
-    )
+function SearchBar(props) {
+  const [searchTerm, setSearchTerm] = useState("");
+  // const[searchTerm, setSearchTerm] = useState('')
+  return (
+    <div className="container">
+      <div className="row ">
+        <div className="col-sm-12 text-center my-4">
+          <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+            <h1 className="font-weight-light">
+              Look up your favorite music for your trip
+            </h1>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Here"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
