fb775a91c8d60928385b74364b9cb1ac4c1b297f
RyanHPaez <solosnail@gmail.com>
2022-08-24 20:11:38 -0700

    added port in env

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const cors = require('cors');
-const uri = process.env.MONGODB_URI
+const mongoose = require("mongoose");
+const cors = require("cors");
+const uri = process.env.MONGODB_URI;
 
-const methodOverride = require ('method-override')
-
-
+const methodOverride = require("method-override");
 
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 //to check put and delete
-app.use(methodOverride('_method'))
-app.use(cors({
-  origin: '*'
-}));
+app.use(methodOverride("_method"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const placeController = require('./src/backend/controller/PlaceController');
-app.use('/app', placeController)
+const placeController = require("./src/backend/controller/PlaceController");
+app.use("/app", placeController);
 
 const userController = require("./src/backend/controller/UserController");
 app.use("/secapp", userController);
